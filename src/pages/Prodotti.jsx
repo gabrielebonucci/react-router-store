68bbf3e6d93128e8ef3fc67bea1308569664e495
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-10-06 16:14:18 +0200

    Integrata API e visualizza lista prodotti

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,6 +1,31 @@
-import React from "react";
-import Navbar from "../components/Navbar";
+import React, { useEffect, useState } from "react";
 const Prodotti = () => {
-  return <div>questa è la pagina prodotti</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error("Errore nel caricamento dei prodotti", error);
+      });
+  }, []); // [] assicura che l'effetto venga eseguito solo una volta
+
+  return (
+    <div>
+      <h1>I Nostri Prodotti</h1>
+      <div className="products-list">
+        {products.map((product) => (
+          <div key={product.id} className="product-card">
+            <img src={product.image} alt={product.title} />
+            <h2>{product.title}</h2>
+            <p>Prezzo: ${product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Prodotti;
